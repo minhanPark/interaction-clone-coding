4f074f0ab96d5b0ce90f723c977cc489454db2f4
Running water <alsgks1218@naver.com>
2020-12-26 20:20:19 +0900

    add current scene check func

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 (() => {
+  let yOffset = 0; //window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된 씬 or 섹션
+
   const sceneInfo = [
     {
       type: "sticky",
@@ -41,8 +45,27 @@
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
-    console.log(sceneInfo);
+  }
+  function scrollLoop() {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+      currentScene--;
+    }
+    console.log(currentScene);
   }
   window.addEventListener("resize", setLayout);
+  window.addEventListener("scroll", () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
   setLayout();
 })();
